e4a251ee33c0a952be3b1585b233840b4b7aa19a
nshosain <nazmul.solutionspin@gmail.com>
2023-01-24 17:34:18 +0600

    add chat send message

--- client/client/src/pages/chat/index.js
+++ client/client/src/pages/chat/index.js
@@ -1,11 +1,13 @@
 import styles from "./styles.module.css";
 import MessagesReceived from "./messages";
+import SendMessage from "./send-message";
 
-const Chat = ({ socket }) => {
+const Chat = ({ username, room, socket }) => {
   return (
     <div className={styles.chatContainer}>
       <div>
         <MessagesReceived socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
       </div>
     </div>
   );
