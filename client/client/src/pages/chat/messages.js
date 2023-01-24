752847895206fa261465d04cafde9978b3a3b5b1
nshosain <nazmul.solutionspin@gmail.com>
2023-01-24 14:23:07 +0600

    impliment socket, room, chat page

---
+++ client/client/src/pages/chat/messages.js
@@ -0,0 +1,49 @@
+import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+const Messages = ({ socket }) => {
+  const [messagesRecieved, setMessagesReceived] = useState([]);
+
+  // Runs whenever a socket event is recieved from the server
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setMessagesReceived((state) => [
+        ...state,
+        {
+          message: data.message,
+          username: data.username,
+          __createdtime__: data.__createdtime__,
+        },
+      ]);
+    });
+
+    // Remove event listener on component unmount
+    return () => socket.off("receive_message");
+  }, [socket]);
+
+  // dd/mm/yyyy, hh:mm:ss
+  function formatDateFromTimestamp(timestamp) {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  }
+
+  return (
+    <div className={styles.messagesColumn}>
+      {messagesRecieved.map((msg, i) => (
+        <div className={styles.message} key={i}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <span className={styles.msgMeta}>{msg.username}</span>
+            <span className={styles.msgMeta}>
+              {formatDateFromTimestamp(msg.__createdtime__)}
+            </span>
+          </div>
+          <p className={styles.msgText}>{msg.message}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Messages;
---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors()); // Add cors middleware
+
+const server = http.createServer(app);
+
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const CHAT_BOT = "ChatBot";
+
+let chatRoom = ""; // E.g. javascript, node,...
+let allUsers = []; // All users in current chat room
+
+// Listen for when the client connects via socket.io-client
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  // Add a user to a room
+  socket.on("join_room", (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+
+    let __createdtime__ = Date.now(); // Current timestamp
+    // Send message to all users currently in the room, apart from the user that just joined
+    socket.to(room).emit("receive_message", {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    // Send welcome msg to only the user that just joined chat
+    socket.emit("receive_message", {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+    chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit("chatroom_users", chatRoomUsers);
+    socket.emit("chatroom_users", chatRoomUsers);
+  });
+});
+
+server.listen(4000, () => "Server is running on port 4000");
