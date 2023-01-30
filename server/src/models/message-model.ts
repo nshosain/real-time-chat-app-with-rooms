73777af0e9a41331e8fa0d39d15acf62f3a280e0
nshosain <nazmul.solutionspin@gmail.com>
2023-01-30 11:35:47 +0600

    update services

---
+++ server/src/models/message-model.ts
@@ -0,0 +1,5 @@
+export interface SaveMessageInterface {
+    message: string,
+    username: string,
+    room: string
+}
--- server/src/services/harper-save-message.ts
+++ server/src/services/harper-save-message.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { SaveMessageInterface } from "../models/message-model";
 
-export default function harperSaveMessage(message, username, room) {
+export default function harperSaveMessage(saveMessage: SaveMessageInterface) {
+
   const dbUrl = process.env.HARPERDB_URL;
   const dbPw = process.env.HARPERDB_PW;
   if (!dbUrl || !dbPw) return null;
@@ -9,13 +11,7 @@ export default function harperSaveMessage(message, username, room) {
     operation: "insert",
     schema: "realtime_chat_app",
     table: "messages",
-    records: [
-      {
-        message,
-        username,
-        room,
-      },
-    ],
+    records: [saveMessage],
   });
 
   var config = {
