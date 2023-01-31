5d75fce67d2423a5bcbfa619df0390f60c64bcda
nshosain <nazmul.solutionspin@gmail.com>
2023-01-31 15:33:23 +0600

    add environment variables

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -5,7 +5,7 @@ import Chat from "./pages/chat";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect(process.env.REACT_APP_BACKEND_SERVER);
 
 function App() {
   const [username, setUsername] = useState("");
